4b46f95d1f02eb01b7d7ba9b5cd59b4edd3e3528
dandy <50742042+temryova@users.noreply.github.com>
2020-03-12 16:08:21 +0200

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,9 +10,9 @@ from blocks.main import MainPage
 @pytest.yield_fixture(scope = "session")
 def driver():
     browser = webdriver.Chrome(executable_path = '/Users/eckoda/Documents/chromedriver')
-    # browser.get('https://doc-stg.telemed.care/')
+    # browser.get('URL HERE')
     login_page = MainPage(browser)
-    login_page.login('930343159', u'укр')
+    login_page.login('NUMBER HERE', u'укр')
     yield browser
 
     browser.close()
